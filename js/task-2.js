eb5488d89615245e7fccbfd4e6205940295c4393
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2024-10-02 17:10:13 -0600

    Add files via upload

---
+++ js/task-2.js
@@ -0,0 +1,38 @@
+"use strict";
+
+class Storage {
+  #items;
+
+  constructor(items){
+    this.#items = items;
+  };
+
+  getItems() { 
+    return this.#items
+  };
+
+  addItem(newItem) { 
+    this.#items.push(newItem)
+  };
+
+  removeItem(itemToRemove) { 
+    const idx = this.#items.indexOf(itemToRemove)
+    if (idx > -1) {
+      this.#items.splice(idx, 1)
+    }
+  };
+
+};
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
