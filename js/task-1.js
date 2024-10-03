a90e59cd6b8cac32f5e91314cf4d445274ea3b22
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2024-10-02 18:11:06 -0600

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,35 +1,32 @@
 "use strict";
 
-class Storage {
-  #items;
-
-  constructor(items){
-    this.#items = items;
-  };
-
-  getItems() { 
-    return this.#items
-  };
-
-  addItem(newItem) { 
-    this.#items.push(newItem)
-  };
-
-  removeItem(itemToRemove) { 
-    this.#items.pop(itemToRemove)
-  };
-
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
 };
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
-
-storage.removeItem("Scaner");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
-
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
